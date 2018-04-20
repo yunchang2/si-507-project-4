1fac7a6a571b92e89e9e4c91fe2dccfc56da4172
yunchang <yunchang@umich.edu>
2018-04-19 21:09:41 -0400

    correct error for exampel

--- secrets_example.py
+++ secrets_example.py
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-# put your google api key here and rename this file to secrets.py
-=======
 # put your Flickr api key here and rename this file to secrets.py
->>>>>>> 37ee092eeb0c6756e5dd8fe2cad0c9acf03d7f7f
 FLICKR_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
